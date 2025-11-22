79170994b3d6b3569a18f6564c14d7a4a376c4d4
pprusaczyk <pawlop1112@gmail.com>
2025-11-22 14:19:09 +0100

    Update foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,23 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector<char> result;
+
+    result.reserve(people.size());
+
+    for (auto it = people.rbegin(); it != people.rend(); ++it)
+    {
+        it->birthday();
+
+        if (it->isMonster())
+        {
+            result.push_back('n');
+        }
+        else
+        {
+            result.push_back('y');
+        }
+    }
+
+    return result;
 }
